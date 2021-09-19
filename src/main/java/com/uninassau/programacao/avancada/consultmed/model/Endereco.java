8a4f09c57a4e0bf0147fb2edeff573c33f1ca9a6
Roberto Coutinho <robertogmcfilho@gmail.com>
2021-09-19 18:43:58 -0300

    Configuração do projeto e criação do primeiro endpoint "endereço" para teste.

---
+++ src/main/java/com/uninassau/programacao/avancada/consultmed/model/Endereco.java
@@ -0,0 +1,35 @@
+package com.uninassau.programacao.avancada.consultmed.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String rua;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false)
+    private String estado;
+
+    @Column(nullable = false)
+    private String cep;
+
+}
