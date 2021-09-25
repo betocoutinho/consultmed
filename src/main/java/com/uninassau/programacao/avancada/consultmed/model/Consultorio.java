40487b1a9e7157ebdbe07f17087d2fad5758f133
Isa Maria Carmo <hamaraisa@gmail.com>
2021-09-25 15:02:20 -0300

    Model/dto/mapper/repository/service/controller

--- src/main/java/com/uninassau/programacao/avancada/consultmed/model/Consultorio.java
+++ src/main/java/com/uninassau/programacao/avancada/consultmed/model/Consultorio.java
@@ -1,4 +1,62 @@
 package com.uninassau.programacao.avancada.consultmed.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Consultorio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(nullable = false)
+    private String nomeFantasia;
+
+    @Column(nullable = false)
+    private String cnpj;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
